d0f33233fb0dff35f2d267573f0ac95be53d0a9b
Zeeshan Siddiqui <zeeshansd767@gmail.com>
2025-10-14 18:15:23 +0500

    commit changes

--- Admin/src/lib/axios.ts
+++ Admin/src/lib/axios.ts
@@ -5,7 +5,7 @@ const api = axios.create({
   baseURL:
     window.location.hostname === "localhost"
       ? "http://localhost:3000/api"          
-      : "https://ims-server-sage.vercel.app", 
+      : "https://ims-server-sage.vercel.app/api", 
   withCredentials: true,
 })
 
