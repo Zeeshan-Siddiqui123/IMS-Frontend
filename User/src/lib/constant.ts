e8886e2a0381e8dd84eba2da155fdfdd6ba90428
muhammadshahms <muhammadshah516@gmail.com>
2025-12-10 00:29:30 +0500

    feat: introduce application name and socket URL constants

--- User/src/lib/constant.ts
+++ User/src/lib/constant.ts
@@ -4,5 +4,5 @@ export const APPNAME = "Incubation Management System"
 export const SOCKET_URL = import.meta.env.VITE_SOCKET_URL || (
     window.location.hostname === "localhost"
         ? "http://localhost:3000"
-        : "https://ims-server-sage.vercel.app"
+        : "https://bqims.vercel.app"
 )
