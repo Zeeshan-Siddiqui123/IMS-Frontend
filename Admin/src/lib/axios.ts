6d16e2e05fb33212951194aea9b2e186ee94fca8
muhammadshahms <muhammadshah516@gmail.com>
2025-12-10 23:29:37 +0500

    feat: configure axios instance with dynamic base URL and credentials

--- Admin/src/lib/axios.ts
+++ Admin/src/lib/axios.ts
@@ -5,7 +5,7 @@ const api = axios.create({
   baseURL:
     window.location.hostname === "localhost"
       ? "http://localhost:3000"
-      : "https://ims-server-sage.vercel.app",
+      : "https://ims-backend-qmis.up.railway.app",
 
   withCredentials: true,
 })
