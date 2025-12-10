ddcbd7b9d011fb17ced8fe1b621403ea8a1d7539
muhammadshahms <muhammadshah516@gmail.com>
2025-12-10 23:37:39 +0500

    hnnv

--- Admin/src/lib/axios.ts
+++ Admin/src/lib/axios.ts
@@ -2,11 +2,7 @@ import axios from "axios"
 
 
 const api = axios.create({
-  baseURL:
-    window.location.hostname === "localhost"
-      ? "http://localhost:3000"
-      : "https://ims-backend-qmis.up.railway.app",
-
+  baseURL: "https://ims-backend-qmis.up.railway.app",
   withCredentials: true,
 })
 
